8b071afc64773ea9461e678483c2902f6ea1cd8e
sleekyom <amdas72@gmail.com>
2023-04-26 00:06:15 +0300

    Add main pages

---
+++ src/pages/ForgotPassword.tsx
@@ -0,0 +1,9 @@
+function ForgotPassword() {
+  return (
+    <div>
+      <h1>Forgot Password page</h1>
+    </div>
+  );
+}
+
+export default ForgotPassword;
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,9 @@
+function SignIn() {
+  return (
+    <div>
+      <h1>Sign In page</h1>
+    </div>
+  );
+}
+
+export default SignIn;
